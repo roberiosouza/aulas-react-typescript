03b653e02e333722480bb5a1be8d7db5174548b7
roberio.dev <rroberiosouza@gmail.com>
2023-05-21 19:49:46 -0300

    Adicionado a rota da página de login

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { Dashboard } from "../pages";
+import { Dashboard, Login } from "../pages";
 
 export const MainRoute = () => {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/pagina-inicial" element={<Dashboard />}></Route>
+                <Route path="/login" element={<Login />}></Route>
                 <Route path="*" element={<Navigate to="/pagina-inicial" />}></Route>
             </Routes>
         </BrowserRouter>
